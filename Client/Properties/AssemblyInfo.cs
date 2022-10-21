5ac14f832213f8e003ea29fe8c7a8bb5247ef448
sardelka9515 <sardelka9515@gmail.com>
2022-10-21 19:43:10 +0800

    Compile

--- Client/Properties/AssemblyInfo.cs
+++ Client/Properties/AssemblyInfo.cs
@@ -5,5 +5,5 @@
 
 using System.Reflection;
 
-[assembly: AssemblyVersion("0.1.547.485")]
-[assembly: AssemblyFileVersion("0.1.547.485")]
+[assembly: AssemblyVersion("0.1.2485.351")]
+[assembly: AssemblyFileVersion("0.1.2485.351")]
